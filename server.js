7c0a658e252b939ea5cf608778f5c4078679e1bc
sanasdh <sanasdh@gmail.com>
2020-08-19 11:55:28 -0400

    database

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
